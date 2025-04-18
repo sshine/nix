c3bf0c15be3fb0cd7acd8628df6649ba9cc31a6d
Simon Shine <simon@simonshine.dk>
2025-04-18 23:06:23 +0000

    feat(common): Add passwordless sudo for wheel group

--- common.nix
+++ common.nix
@@ -1,7 +1,14 @@
 { pkgs, config, lib, ... }: {
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
+
+  security.sudo = {
+    enable = true;
+    execWheelOnly = true;
+    wheelNeedsPassword = false;
+  };
+
   services.openssh.enable = true;
 }
