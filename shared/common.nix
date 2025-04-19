f9999b2be57dbbcaf6d1aed49282412dd7d0309c
Simon Shine <simon@simonshine.dk>
2025-04-19 00:10:12 +0000

    feat(common): Add common programs, set EDITOR=vim, allowUnfree = true;

--- shared/common.nix
+++ shared/common.nix
@@ -4,7 +4,7 @@
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  nixpkgs.config.allowUnfree = true;
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
 
@@ -15,4 +15,31 @@
   };
 
   services.openssh.enable = true;
+
+  environment.variables.EDITOR = "vim";
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    curl
+    htop
+    killall
+    screen
+    jq
+    just
+    ripgrep
+    xz
+    zip
+    unzip
+    zstd
+    gnutar
+    file
+    which
+    tree
+    bat
+    dnsutils
+    openssl
+    watchexec
+    nmap
+  ];
 }
