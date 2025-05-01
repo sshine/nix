19069f20f2819e336b4929de02272aba8af2c207
Simon Shine <simon@simonshine.dk>
2025-05-01 23:34:38 +0200

    feat(terranix): Add 'package' option for switching between opentofu/terraform

--- programs/terranix.nix
+++ programs/terranix.nix
@@ -1,13 +1,19 @@
 { pkgs, lib, config, ... }: {
   options = {
     programs.terranix.enable = lib.mkEnableOption "terranix";
+    programs.terranix.package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.opentofu;
+      example = "pkgs.terraform";
+      description = "The package to use for Terraform/OpenTofu functionality.";
+    };
   };
 
   config = lib.mkIf config.programs.terranix.enable
     {
       environment.systemPackages = [
-        pkgs.terraform
         pkgs.terranix
+        config.programs.terranix.package
       ];
     };
 }
