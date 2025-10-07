6b11aeacc296bc4cc9d10bbdaa6f35cd861a9e93
Simon Shine <simon@simonshine.dk>
2025-10-07 09:07:18 +0200

    feat(users): Add derp15 ssh key

--- shared/users.nix
+++ shared/users.nix
@@ -16,6 +16,7 @@
         ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+WHf94v0BWIlOlPZZVnlWUgx0+s4EgusbXdWRoLvYD''
         ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJqN6vKRZfPx4QOkQoIsJ6vJbiclaU9opbYLdzzk7bY''
         ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFWF+CfMuGgMsnqz8QWcNpxdx5j63UT6Mh7WCqvTH6o5 sshine@m1''
+        ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUMtKxIrZdXFRBRjHhI5o/loiu6HG0Yb87Ots5FBQH2 sshine@derp15''
       ];
       description = "SSH public keys shared between root and primary super-user account";
     };
